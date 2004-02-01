f4016240f45f4bed145f7767459f26a901e35645
Fredrik Ehnbom <quarnster@gmail.com>
2004-02-01 16:09:41 +0000

    digital pad + direct rendering

--- ntscmenu.cc
+++ ntscmenu.cc
@@ -3,12 +3,13 @@
 
 extern pvr_poly_cxt_t loadImage(char *, int);
 NtscMenu::NtscMenu() {
-	pvr_poly_cxt_t cxt = loadImage("palntsc.png", PVR_LIST_TR_POLY);
+	pvr_poly_cxt_t cxt = loadImage("palntsc.png", PVR_LIST_OP_POLY);
 	pvr_poly_compile(&header, &cxt);
 }
 
 NtscMenu::~NtscMenu() {
-	pvr_mem_free(addr);
+	printf("TODO: free pal/ntsc-image\n");
+//	pvr_mem_free(addr);
 }
 
 void NtscMenu::showMenu() {
@@ -17,34 +18,33 @@ void NtscMenu::showMenu() {
 	float time = 0;
 	float framerate = 1.0;
 
+	pvr_dr_state_t state;
+	pvr_dr_init(state);
+	pvr_vertex_t *vert;
+
 	while (!done) {
 		MAPLE_FOREACH_BEGIN(MAPLE_FUNC_CONTROLLER, cont_state_t, st)
 			if (st->buttons & CONT_START || st->buttons & CONT_A)
 				done = true;
-			// TODO: change item on up/down
-			if (st->joyy < -64 && choice != 0) {
+			if ((st->buttons & CONT_DPAD_UP || st->joyy < -64) && choice != 0) {
 				choice = 0;
 				framerate = 1.0f;
 				vid_set_mode(DM_640x480_PAL_IL,PM_RGB565);
-			} else if (st->joyy > 64 && choice != 1) {
+			} else if ((st->buttons & CONT_DPAD_DOWN || st->joyy > 64) && choice != 1) {
 				choice = 1;
 				framerate = 50.0f / 60.0f;
 				vid_set_mode(DM_640x480_NTSC_IL, PM_RGB565);
 			}
-			if (st->joy2y != 0) {
-				printf("!= 0: %d\n", st->joy2y);
-			}
 		MAPLE_FOREACH_END()
 
+
 		// begin render with TA
+		pvr_wait_ready();
 		pvr_scene_begin();
-		pvr_list_begin(PVR_LIST_TR_POLY);
+		pvr_list_begin(PVR_LIST_OP_POLY);
 
 		pvr_prim(&header, sizeof(pvr_poly_hdr_t));
 
-		pvr_vertex_t vert;
-		vert.argb = PVR_PACK_COLOR(1.0f, 1.0f, 1.0f, 1.0f);
-		vert.z = 1 / 1.10f;
 
 		float xadd = fsin(time * 1.1) * 10;
 		float yadd = fsin(time * 1.3) * 10;
@@ -54,29 +54,40 @@ void NtscMenu::showMenu() {
 			yadd = 0;
 		}
 
-		vert.flags = PVR_CMD_VERTEX;
-		vert.u = 0; vert.v = 0.5;
-		vert.x = 192-xadd; vert.y = 272-64+yadd;
-		pvr_prim(&vert, sizeof(pvr_vertex_t));
+		vert = pvr_dr_target(state);
+		vert->flags = PVR_CMD_VERTEX;
+		vert->argb = PVR_PACK_COLOR(1.0f, 1.0f, 1.0f, 1.0f);
+		vert->z = 1 / 1.10f;
+		vert->u = 0; vert->v = 0.5;
+		vert->x = 192-xadd; vert->y = 272-64+yadd;
+		pvr_dr_commit(vert);
 
-		vert.u = 0; vert.v = 0;
-		vert.x = 192-xadd; vert.y = 208-64-yadd;
-		pvr_prim(&vert, sizeof(pvr_vertex_t));
+		vert = pvr_dr_target(state);
+		vert->flags = PVR_CMD_VERTEX;
+		vert->argb = PVR_PACK_COLOR(1.0f, 1.0f, 1.0f, 1.0f);
+		vert->z = 1 / 1.10f;
+		vert->u = 0; vert->v = 0;
+		vert->x = 192-xadd; vert->y = 208-64-yadd;
+		pvr_dr_commit(vert);
 
-		vert.u = 1; vert.v = 0.5;
-		vert.x = 448+xadd; vert.y = 272-64+yadd;
-		pvr_prim(&vert, sizeof(pvr_vertex_t));
+		vert = pvr_dr_target(state);
+		vert->flags = PVR_CMD_VERTEX;
+		vert->argb = PVR_PACK_COLOR(1.0f, 1.0f, 1.0f, 1.0f);
+		vert->z = 1 / 1.10f;
+		vert->u = 1; vert->v = 0.5;
+		vert->x = 448+xadd; vert->y = 272-64+yadd;
+		pvr_dr_commit(vert);
 
-		vert.flags = PVR_CMD_VERTEX_EOL;
-		vert.u = 1; vert.v = 0;
-		vert.x = 448+xadd; vert.y = 208-64-yadd;
-		pvr_prim(&vert, sizeof(pvr_vertex_t));
+		vert = pvr_dr_target(state);
+		vert->argb = PVR_PACK_COLOR(1.0f, 1.0f, 1.0f, 1.0f);
+		vert->flags = PVR_CMD_VERTEX_EOL;
+		vert->z = 1 / 1.10f;
+		vert->u = 1; vert->v = 0;
+		vert->x = 448+xadd; vert->y = 208-64-yadd;
+		pvr_dr_commit(vert);
 
 
 		// ntsc
-		vert.argb = PVR_PACK_COLOR(1.0f, 1.0f, 0.0f, 0.0f);
-		vert.z = 1 / 1.10f;
-
 		xadd = fsin(time * 1.1) * 10;
 		yadd = fsin(time * 1.3) * 10;
 
@@ -85,29 +96,42 @@ void NtscMenu::showMenu() {
 			yadd = 0;
 		}
 
-		vert.flags = PVR_CMD_VERTEX;
-		vert.u = 0; vert.v = 1;
-		vert.x = 192-xadd; vert.y = 272+64+yadd;
-		pvr_prim(&vert, sizeof(pvr_vertex_t));
+		vert = pvr_dr_target(state);
+		vert->argb = PVR_PACK_COLOR(1.0f, 1.0f, 0.0f, 0.0f);
+		vert->z = 1 / 1.10f;
+		vert->flags = PVR_CMD_VERTEX;
+		vert->u = 0; vert->v = 1;
+		vert->x = 192-xadd; vert->y = 272+64+yadd;
+		pvr_dr_commit(vert);
 
-		vert.u = 0; vert.v = 0.5;
-		vert.x = 192-xadd; vert.y = 208+64-yadd;
-		pvr_prim(&vert, sizeof(pvr_vertex_t));
+		vert = pvr_dr_target(state);
+		vert->argb = PVR_PACK_COLOR(1.0f, 1.0f, 0.0f, 0.0f);
+		vert->z = 1 / 1.10f;
+		vert->flags = PVR_CMD_VERTEX;
+		vert->u = 0; vert->v = 0.5;
+		vert->x = 192-xadd; vert->y = 208+64-yadd;
+		pvr_dr_commit(vert);
 
-		vert.u = 1; vert.v = 1;
-		vert.x = 448+xadd; vert.y = 272+64+yadd;
-		pvr_prim(&vert, sizeof(pvr_vertex_t));
+		vert = pvr_dr_target(state);
+		vert->argb = PVR_PACK_COLOR(1.0f, 1.0f, 0.0f, 0.0f);
+		vert->z = 1 / 1.10f;
+		vert->flags = PVR_CMD_VERTEX;
+		vert->u = 1; vert->v = 1;
+		vert->x = 448+xadd; vert->y = 272+64+yadd;
+		pvr_dr_commit(vert);
 
-		vert.flags = PVR_CMD_VERTEX_EOL;
-		vert.u = 1; vert.v = 0.5;
-		vert.x = 448+xadd; vert.y = 208+64-yadd;
-		pvr_prim(&vert, sizeof(pvr_vertex_t));
+		vert = pvr_dr_target(state);
+		vert->argb = PVR_PACK_COLOR(1.0f, 1.0f, 0.0f, 0.0f);
+		vert->z = 1 / 1.10f;
+		vert->flags = PVR_CMD_VERTEX_EOL;
+		vert->u = 1; vert->v = 0.5;
+		vert->x = 448+xadd; vert->y = 208+64-yadd;
+		pvr_dr_commit(vert);
 
 
 		pvr_list_finish();
 
 		pvr_scene_finish();
-		pvr_wait_ready();
 
 		time += 1 / 10.0f * framerate;
 	}
