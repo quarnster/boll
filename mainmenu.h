ff0a98d2fee92c7566cc6a3b612ed03520a97d9b
Fredrik Ehnbom <quarnster@gmail.com>
2004-02-25 08:19:36 +0000

    removed plx-stuff, included resources

--- mainmenu.h
+++ mainmenu.h
@@ -1,11 +1,8 @@
 #ifndef __INCLUDED_MAINMENU_H
 #define __INCLUDED_MAINMENU_H
 
-#include <plx/font.h>
-#include <plx/list.h>
-#include <plx/dr.h>
-
 #include "menu.h"
+#include "resources.h"
 
 class MainMenu : public Menu {
 public:
